59c993802ee1171bd90de7431cfd90a122605581
Mantas Bertasius <33432592+mantasbertas@users.noreply.github.com>
2018-05-15 00:54:20 +0300

    .js
    added .js file

---
+++ scripts.js
@@ -0,0 +1,56 @@
+
+  var x;
+  var y = false;
+  var x = "We won \"zulull\"";
+  var didwewin = (y===true)?"Yas":"Nos";
+  var string1 = "Suck ";
+  var string2 = "Me Dick";
+  var string3 ="ddidntwinzullulo";
+  var z = 0;
+
+
+
+
+  if (y && x) {
+      document.write(string1+string2);
+      //alert(x);
+      //alert(didwewin);
+  }
+  else if (x) {
+    for (var i = -2; i < 3; i++) {
+    //
+      if(i=-1){
+        alert(didwewin);
+        break;
+        }
+      if(i=0){
+        continue;
+        }
+    }
+  }
+  else {
+    alert(string3);
+  }
+
+
+  do {
+    document.write("ajajajajai puerto rico<br/>");
+    z++;
+  } while (z<3);
+
+  switch (z) {
+    case 1:
+      alert("this is sux");
+      break;
+    case 2:
+      alert("this is also sux");
+      break;
+    case 3:
+      alert("how do i read");
+      break;
+    default:
+      alert("you shoulnt be seing this");
+  }
+
+
+  //nothing
